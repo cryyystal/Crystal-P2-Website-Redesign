140c023f3c60c1d96401ce1a79362d01357e77c0
Crystal Wong <crystalwong-cw@hotmail.com>
2021-12-04 19:42:07 -0500

    a

---
+++ Crystal-P2-Website-Redesign.js
@@ -0,0 +1,16 @@
+// <div id="biotext" style="display: block; top: 6px; left: 234px;">
+// 			<p>put text here</p>
+// 			</div>
+
+// #biotext{
+// 	display: none;
+// 	position: absolute;
+// 	background-color: #f2f2f2;
+// 	opacity: 0.9;
+// 	width: 400px;
+// 	height: fit-content;
+// 	padding: 25px;
+// 	border-radius: 10px;
+// 	left:0;
+// 	top:0;
+// }
