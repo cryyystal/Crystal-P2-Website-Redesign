b5c3cd6743b450a285a58a54c766eb25f37f2c03
Crystal Wong <crystalwong-cw@hotmail.com>
2021-12-09 01:22:42 -0500

    all branches added +
    file name changes, new files added and organized, units changed, etc

--- Crystal-P2-Website-Redesign.js
+++ Crystal-P2-Website-Redesign.js
@@ -1,33 +1,3 @@
-// <div id="biotext" style="display: block; top: 6px; left: 234px;">
-// 			<p>put text here</p>
-// 			</div>
-
-// #biotext{
-// 	display: none;
-// 	position: absolute;
-// 	background-color: #f2f2f2;
-// 	opacity: 0.9;
-// 	width: 400px;
-// 	height: fit-content;
-// 	padding: 25px;
-// 	border-radius: 10px;
-// 	left:0;
-// 	top:0;
-// }
-
-
-
-// <!--      <div class="library-branches">
-//       <h2>Library Branches</h2>       
-//      </div> -->
-
-// <li><a href="put HTML page name">Put text here</a> </li>
-
-	// color: black;
-	// font-family: tuppence,serif;
-	// font-weight: 900;
-	// font-style: normal;
-
 var slideIndex = 0;
 showSlides();
 
